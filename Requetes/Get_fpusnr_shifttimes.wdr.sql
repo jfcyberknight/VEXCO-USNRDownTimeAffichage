811ab9b3116a50cc135d819a23beedfb20ae17ec
Vallée Jean-François <vallee.jeanfrancois@gmail.com>
2026-05-08 15:01:35 -0400

    refactor(schema): synchronize codebase with corrected shifttimes table structure

--- Requetes/Get_fpusnr_shifttimes.wdr.sql
+++ Requetes/Get_fpusnr_shifttimes.wdr.sql
@@ -1,4 +1,4 @@
 -- Requête pour fpusnr_shifttimes
-SELECT ShiftId, MondayStart, MondayEnd, TuesdayStart, TuesdayEnd, WednesdayStart, WednesdayEnd, ThursdayEnd, FridayStart, SaturdayStart, SaturdayEnd, SundayStart, SundayEnd, manualOverride, isCached
+SELECT ShiftId, MondayStart, MondayEnd, TuesdayStart, TuesdayEnd, WednesdayStart, WednesdayEnd, ThursdayStart, ThursdayEnd, FridayStart, FridayEnd, SaturdayStart, SaturdayEnd, SundayStart, SundayEnd, manualOverride, isCached
 FROM fpusnr_shifttimes
 WHERE 1=1
--- migrations/20260508_fix_all_days_shift_start.sql
+++ migrations/20260508_fix_all_days_shift_start.sql
@@ -1,40 +1,31 @@
 -- Migration: 20260508_fix_all_days_shift_start.sql
--- Description: Ajustement de l'heure de début du quart de travail pour tous les jours de semaine (7h00 -> 6h01).
--- Note: Le schéma semble avoir des noms de colonnes inversés ou erronés (ex: ThursdayEnd utilisé pour le début du jeudi).
+-- Description: Ajustement de l'heure de début du quart de travail pour les jours de semaine (7h00 -> 6h01).
+-- Structure corrigée : Utilise ThursdayStart et FridayStart.
 -- Architecte: Senior Software Architect
 
 -- 1. Vérification avant mise à jour
 SELECT 
     ShiftId, 
-    MondayStart, 
-    TuesdayStart, 
-    WednesdayStart, 
-    ThursdayEnd AS ThursdayStart_Candidate, -- Suspecté d'être le début du Jeudi
-    FridayStart,
-    SundayEnd AS FridayEnd_Candidate -- Suspecté d'être la fin du Vendredi ou début ?
+    MondayStart, TuesdayStart, WednesdayStart, ThursdayStart, FridayStart
 FROM fpusnr_shifttimes
 WHERE ShiftId = 1;
 
--- 2. Mise à jour de l'heure de début pour tous les jours de semaine
--- On applique 06:01:00 à toutes les colonnes identifiées comme gérant les débuts de quarts.
+-- 2. Mise à jour de l'heure de début pour les jours de semaine uniquement
+-- On cible le quart de "Jour" (généralement ShiftId = 1)
 
 UPDATE fpusnr_shifttimes
 SET 
     MondayStart = CAST(CONVERT(VARCHAR(10), MondayStart, 120) + ' 06:01:00' AS DATETIME2),
     TuesdayStart = CAST(CONVERT(VARCHAR(10), TuesdayStart, 120) + ' 06:01:00' AS DATETIME2),
     WednesdayStart = CAST(CONVERT(VARCHAR(10), WednesdayStart, 120) + ' 06:01:00' AS DATETIME2),
-    ThursdayEnd = CAST(CONVERT(VARCHAR(10), ThursdayEnd, 120) + ' 06:01:00' AS DATETIME2), -- Correction pour le Jeudi
+    ThursdayStart = CAST(CONVERT(VARCHAR(10), ThursdayStart, 120) + ' 06:01:00' AS DATETIME2),
     FridayStart = CAST(CONVERT(VARCHAR(10), FridayStart, 120) + ' 06:01:00' AS DATETIME2)
 WHERE ShiftId = 1;
 
 -- 3. Vérification après mise à jour
 SELECT 
     ShiftId, 
-    MondayStart, 
-    TuesdayStart, 
-    WednesdayStart, 
-    ThursdayEnd AS ThursdayStart_Updated,
-    FridayStart
+    MondayStart, TuesdayStart, WednesdayStart, ThursdayStart, FridayStart
 FROM fpusnr_shifttimes
 WHERE ShiftId = 1;
 
